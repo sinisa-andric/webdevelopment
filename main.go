6b2f536f44761f247a2b054d15b5e3f07f233c6a
sinisa-andric <sinisa.andric@mail.ru>
2022-02-17 20:51:02 +0100

    add contact page/route

--- main.go
+++ main.go
@@ -11,9 +11,13 @@ func handlerFunction(writter http.ResponseWriter, reader *http.Request) {
 
 	// log
 	fmt.Println("Someone visited oru page")
+	fmt.Println(writter, reader.URL.Path)
 
-	fmt.Fprint(writter, "<h1> Welcome to my website! HOT RELOAD </h1>")
-
+	if reader.URL.Path == "/" {
+		fmt.Fprint(writter, "<h1> Welcome to my website! </h1>")
+	} else if reader.URL.Path == "/contact" {
+		fmt.Fprint(writter, "To get in touch, please send an email to <a href=\"mailto:sinisa.andric@mail.ru\"> sinisa.andric@mail.ru")
+	}
 }
 
 func main() {
